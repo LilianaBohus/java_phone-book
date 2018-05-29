b418bad87d88aaf4a1a43cc6aef98896415d3d17
LilianaBohus <boli96@gmail.com>
2018-05-29 15:53:59 +0200

    Implements ctor, getters and setters

--- TelefonkonyvApp/src/Nevjegy.java
+++ TelefonkonyvApp/src/Nevjegy.java
@@ -5,7 +5,8 @@ public class Nevjegy {
 	private String foglalkozas;
 	private String lakcim;
 	private String email;
-	
+
+	// TIPP: Getter es Setter fuggvenyeket legeneralni a Source --> Generate Getters and Setters paranccsal tudunk.
 	public String getNev() {
 		return nev;
 	}
@@ -31,12 +32,24 @@ public class Nevjegy {
 		this.email = email;
 	}
 	
+	// TIPP: Konstruktort generalni a Source --> Generate Constructor using fields... paranccsal tudunk.
 	public Nevjegy(String nev, String foglalkozas, String lakcim, String email) {
 		super();
 		this.nev = nev;
 		this.foglalkozas = foglalkozas;
 		this.lakcim = lakcim;
 		this.email = email;
+	}
+	
+	// Osszehasonlitja a nevjegyet, amire meghivjak, a parameterben levovel, hogy egyeznek-e az attributumaik.
+	public boolean equals(Nevjegy n) {
+		if (n.getNev().equals(this.getNev()) && 
+			n.getFoglalkozas().equals(this.getFoglalkozas()) && 
+			n.getLakcim().equals(this.getLakcim()) &&
+			n.getEmail().equals(this.getEmail())){
+			return true;
+		}
+		return false;
 	}
 
 }
